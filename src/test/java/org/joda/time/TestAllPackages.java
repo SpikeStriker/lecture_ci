91f8653681c37dfa8c4389578d50904a45fbe6c3
Uday Jain <uday9k@gmail.com>
2024-07-26 10:15:26 +0200

    adding back all tests

--- src/test/java/org/joda/time/TestAllPackages.java
+++ src/test/java/org/joda/time/TestAllPackages.java
@@ -33,13 +33,13 @@ public class TestAllPackages extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite();
-//      suite.addTest(org.joda.time.TestAll.suite());
-//      suite.addTest(org.joda.time.chrono.TestAll.suite());
-//      suite.addTest(org.joda.time.chrono.gj.TestAll.suite());
-//      suite.addTest(org.joda.time.convert.TestAll.suite());
-//      suite.addTest(org.joda.time.field.TestAll.suite());
-//      suite.addTest(org.joda.time.format.TestAll.suite());
-//      suite.addTest(org.joda.time.tz.TestAll.suite());
+        suite.addTest(org.joda.time.TestAll.suite());
+        suite.addTest(org.joda.time.chrono.TestAll.suite());
+        suite.addTest(org.joda.time.chrono.gj.TestAll.suite());
+        suite.addTest(org.joda.time.convert.TestAll.suite());
+        suite.addTest(org.joda.time.field.TestAll.suite());
+        suite.addTest(org.joda.time.format.TestAll.suite());
+        suite.addTest(org.joda.time.tz.TestAll.suite());
         return suite;
     }
 
